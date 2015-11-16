16825ae1f88a5294a7f0d9ad4406758ee51d3e05
a-komarev <ell@cybercog.su>
2015-11-16 03:21:06 +0300

    Try to download on click

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,17 +1,6 @@
-function saveFile(url) {
-    // Get file name from url.
-    var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = function () {
-        var a = document.createElement('a');
-        a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
-        a.download = filename; // Set the file name.
-        a.style.display = 'none';
-        document.body.appendChild(a);
-        a.click();
-        delete a;
-    };
-    xhr.open('GET', url);
-    xhr.send();
+function saveImageAs(imgOrURL) {
+    if (typeof imgOrURL == 'object')
+        imgOrURL = imgOrURL.src;
+    window.win = open (imgOrURL);
+    setTimeout('win.document.execCommand("SaveAs")', 500);
 }
