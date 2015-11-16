378f03189c048e00fd0d188f109d2c9a25bb7843
a-komarev <ell@cybercog.su>
2015-11-16 03:23:28 +0300

    Try to download on click

--- public/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function saveImageAs(imgOrURL) {
-    if (typeof imgOrURL == 'object')
-        imgOrURL = imgOrURL.src;
-    window.win = open (imgOrURL);
-    setTimeout('win.document.execCommand("SaveAs")', 500);
-}
